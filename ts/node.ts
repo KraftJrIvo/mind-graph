ceb8d35f153ecf471d914b4a6ecd120816dc81b5
Ivan <kraftjrivo@gmail.com>
2024-11-14 13:53:30 +0300

    disable caching for html request

--- ts/node.ts
+++ ts/node.ts
@@ -135,6 +135,9 @@ export class Node {
                         if (!this.contentLoading) {
                             this.contentLoading = true
                             let xhr = new XMLHttpRequest()
+							xhr.setRequestHeader('Cache-Control', 'no-cache, no-store, max-age=0')
+							xhr.setRequestHeader('Expires', 'Thu, 1 Jan 1970 00:00:00 GMT')
+							xhr.setRequestHeader('Pragma', 'no-cache')
                             xhr.open('GET', `https://kraftjrivo.github.io/mind-graph/content/${this.nom}.html`)
                             xhr.send()
                             xhr.onload = () => {
